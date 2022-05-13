c30fa6f5c2077fe5ef7601805a24e9ecc296424f
davermeca <90122290+davermeca@users.noreply.github.com>
2022-05-12 21:31:12 -0600

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "TEST_ENTERPRISSE"
+    workspaces {
+      name = "Terraform_test"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server2"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-0cd7c314c94dd8987"
+
+  ingress_cidr_blocks = ["192.168.20.0/24"]
+}
